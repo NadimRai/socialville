5c39b0448fd2a455319b87f0f3aaf1f32dfdb5df
NadimRai <rai_nadim@yahoo.com>
2017-01-18 17:46:14 -0800

    Added user_id to Status

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+   has_many :statuses
+
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", 
-  micro: "40x40>" }, default_url:  "/images/:style/missing.png"  
+  micro: "40x40>" }, default_url:  "ina.jpeg"  
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(username: "BabaBooey",
+                   email: "b@example.com", 
+                   password: "password", 
+                   password_confirmation: "password"
+                   )
+                   
+
+user = User.create(username: "Sol",
+                   email: "sol@example.com", 
+                   password: "password", 
+                   password_confirmation: "password"
+                   )
+                   
+user = User.create(username: "Eric The Actor",
+                 email: "eric@example.com", 
+                 password: "password", 
+                 password_confirmation: "password"
+                 )

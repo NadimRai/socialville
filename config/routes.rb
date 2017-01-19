a92a6432398a4d3abf6e14c5909682fdd9013e33
NadimRai <rai_nadim@yahoo.com>
2017-01-18 20:11:13 -0800

    Created Friendship Model Controller View

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   resources :statuses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'statuses#index'
+  # Making Friends ---------------------
+  resources :users, only: [:index, :show]    
+   resources :friendships, only: [:create, :destroy, :accept] do 
+    member do 
+      put :accept
+    end
+  end
+    # Making Friends ---------------------
 end
